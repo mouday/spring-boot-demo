79a026526c32e7843ff10100c76a03e41822e2e8
pengshiyu <1940607002@qq.com>
2021-09-26 17:21:03 +0800

    fix

---
+++ Spring-Boot-Mail/src/main/java/com/example/demo/service/FreeMarkerService.java
@@ -0,0 +1,23 @@
+package com.example.demo.service;
+
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.IOException;
+import java.util.Map;
+
+@Service
+public class TemplateService {
+    //发送邮件的模板引擎
+    @Autowired
+    private FreeMarkerConfigurer configurer;
+
+    public String renderTemplate(String templateName, Map<String, Object> model) throws IOException, TemplateException {
+        Template template = configurer.getConfiguration().getTemplate(templateName);
+        return FreeMarkerTemplateUtils.processTemplateIntoString(template, model);
+    }
+}
